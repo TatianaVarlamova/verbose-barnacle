24c65f08f5f3c2a795f67edb45928a133f62f410
Tatiana <zippy.2017@mail.ru>
2023-01-14 15:14:02 +0300

    создание цикла

--- Rabota/Program.cs
+++ Rabota/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.WriteLine("Введите числа или слова через пробел: ");
 string inputText = Console.ReadLine()!; 
 string[] stringArray = inputText.Split(' ');
-
+for (int i = 0; i < stringArray.Length; i++)
+{
+    
+}
